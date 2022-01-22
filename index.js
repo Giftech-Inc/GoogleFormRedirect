124e75963cd82cc22480cadcc846851aa27b50ce
hirumaki <index3583@gmail.com>
2022-01-22 09:23:27 +0900

    add form

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('hello world');
+$(document).ready(function () {
+  $('#form').submit(function (event) {
+    var formData = $('#form').serialize();
+    $.ajax({
+      url: 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSdLdri6qyTj1apAxwKJv1ovBW0Rb_YWWbUrS1DpKk8f1Sz0og/formResponse',
+      data: formData,
+      type: 'POST',
+      dataType: 'xml',
+      statusCode: {
+        0: function () {
+          // $(".end-message").slideDown();
+          // $(".submit-btn").fadeOut();
+          window.location.href = 'https://editor-camp.jp/law/thanks.html';
+        },
+        200: function () {
+          // $(".false-message").slideDown();
+        },
+      },
+    });
+    event.preventDefault();
+  });
+});
